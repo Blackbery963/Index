a7273cbaca4bf1aba56ac9ab9f5b1d1cca863eba
blackbery963 <swarnadipb727@gmail.com>
2025-06-05 23:04:55 +0530

    Painters' Diary

--- Index/vite.config.js
+++ Index/vite.config.js
@@ -11,6 +11,11 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   build: {
+    rollupOptions: {
+      external: ['react-ga4'],
+    },
+  },
   optimizeDeps: {
     include: ['swiper'],
     exclude: ['react-hot-toast']

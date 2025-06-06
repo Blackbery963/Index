61c8ee229c78be8cc18f4a67abb5da75bf72468d
blackbery963 <swarnadipb727@gmail.com>
2025-06-06 13:09:43 +0530

    Painters' Diary

--- Index/vite.config.js
+++ Index/vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-   build:{
-   rollupOptions:{
-   external:['aos', 'framer-motion']
-}   
-  },
   optimizeDeps: {
     include: ['swiper','aos', 'framer-motion'],        // ✅ Ensures Swiper is pre-bundled
     exclude: ['react-hot-toast'] // ✅ You exclude it from optimization
